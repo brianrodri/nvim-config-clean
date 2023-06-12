3b40f4071fa7fcf6c50279c8d2c0fd339732d00f
Brian Rodriguez <brianrz@amazon.com>
2023-06-11 21:23:31 -0400

    initial commit with packer auto-install

---
+++ init.lua
@@ -0,0 +1,21 @@
+local ensure_packer = function()
+    local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+        vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd.packadd("packer.nvim")
+        return true
+    end
+    return false
+end
+
+local bootstrapping = ensure_packer()
+local packer = require("packer")
+
+return packer.startup(function(use)
+    -- Manage packer with packer.
+    use { "wbthomason/packer.nvim" }
+
+    -- Automatically set up from the configuration after cloning packer.nvim.
+    -- Keep this at the end after all plugins.
+    if bootstrapping then packer.sync() end
+end)
