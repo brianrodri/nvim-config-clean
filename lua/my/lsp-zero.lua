1ab2186bbce9d00b4c468a36b7212b76487c4b13
Brian Rodriguez <brianrz@amazon.com>
2023-06-24 14:11:32 -0400

    Improve completion mappings and presentation

--- lua/my/lsp-zero.lua
+++ lua/my/lsp-zero.lua
@@ -1,6 +1,4 @@
-local M = {}
-
-M.setup = function()
+local setup_lsp_zero = function()
     local lsp = require("lsp-zero").preset({})
 
     lsp.on_attach(function(_, bufnr)
@@ -14,4 +12,27 @@ M.setup = function()
     lsp.setup()
 end
 
-return M
+local setup_cmp = function()
+    local cmp = require("cmp")
+    local cmp_action = require("lsp-zero").cmp_action()
+
+    cmp.setup({
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
+        mapping = {
+            ['<CR>'] = cmp.mapping.confirm { select = false },
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<Tab>'] = cmp_action.luasnip_supertab(),
+            ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+        }
+    })
+end
+
+return {
+    setup = function()
+        setup_lsp_zero()
+        setup_cmp()
+    end,
+}
