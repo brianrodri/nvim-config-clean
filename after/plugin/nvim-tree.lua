cfe0bf7685d72d66d7ce8e6ead17669b453fbf63
Brian Rodriguez <brianrz@amazon.com>
2023-06-25 01:35:26 -0400

    Consistent style

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,4 +1,4 @@
-require("nvim-tree").setup({
+require("nvim-tree").setup {
     sort_by = "case_sensitive",
     view = {
         width = 30,
@@ -9,4 +9,4 @@ require("nvim-tree").setup({
     filters = {
         dotfiles = true,
     },
-})
+}
