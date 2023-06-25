0d119b7afcccec6240ef47f5fc09312331e6ca78
Brian Rodriguez <brianrz@amazon.com>
2023-06-25 01:34:45 -0400

    Consistent plugin setups

--- lua/my/packer.lua
+++ lua/my/packer.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.setup_packer(use)
+function M.setup_plugins(use)
     use "wbthomason/packer.nvim"
 
     M.setup_colorscheme_plugins(use)
@@ -15,57 +15,44 @@ function M.setup_packer(use)
 end
 
 function M.setup_colorscheme_plugins(use)
-    -- Gruvbox colorscheme
-    use { "eddyekofo94/gruvbox-flat.nvim" }
+    use "eddyekofo94/gruvbox-flat.nvim"
 end
 
 function M.setup_file_plugins(use)
-    -- Intuitive tree explorer
-    use { "nvim-tree/nvim-tree.lua" }
+    use "nvim-tree/nvim-tree.lua"
 end
 
 function M.setup_git_plugins(use)
-    -- Perfect Git plugin
-    use { "tpope/vim-fugitive" }
-
-    -- Git gutter
-    use { "airblade/vim-gitgutter" }
+    use "tpope/vim-fugitive"
+    use "airblade/vim-gitgutter"
 end
 
 function M.setup_indentation_plugins(use)
-    -- Auto-detect indentation style from file
-    use {
-        "nmac427/guess-indent.nvim",
-        config = function() require("guess-indent").setup {} end,
-    }
+    use "nmac427/guess-indent.nvim"
 end
 
 function M.setup_lsp_plugins(use)
-    -- Enhance syntax tree of neovim (depended on by most plugins)
-    use { "nvim-treesitter/nvim-treesitter", run = function() pcall(vim.fn.TSUpdate) end }
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = function() pcall(vim.fn.TSUpdate) end,
+    }
 
-    -- Intuitive LSP resolver
     use {
         "VonHeikemen/lsp-zero.nvim",
         branch = "v2.x",
         requires = {
-            -- LSP Support
-            {"neovim/nvim-lspconfig"},             -- Required
-            {"williamboman/mason.nvim", run = function() pcall(vim.cmd.MasonUpdate) end}, -- Required
-            {"williamboman/mason-lspconfig.nvim"}, -- Optional
-            -- Autocompletion
-            {"hrsh7th/nvim-cmp"},     -- Required
-            {"hrsh7th/cmp-nvim-lsp"}, -- Required
-            {"L3MON4D3/LuaSnip"},     -- Required
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
         },
     }
 
-    -- Perfect lua/vim LSP
-    use { "folke/neodev.nvim" }
+    use "folke/neodev.nvim" -- Configures lua_ls
 end
 
 function M.setup_search_plugins(use)
-    -- Intuitive FZF experience
     use {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.1",
@@ -74,24 +61,28 @@ function M.setup_search_plugins(use)
 end
 
 function M.setup_window_plugins(use)
-    -- Pretty IDE-like buffer tabs
     use { "akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" }
 end
 
 function M.setup_statusline_plugins(use)
-    -- Pretty status line
-    use { "nvim-lualine/lualine.nvim" }
+    use "nvim-lualine/lualine.nvim"
 end
 
 function M.setup_tmux_plugins(use)
-    -- Tmux/vim window interactions
-    use {
-        "aserowy/tmux.nvim",
-        config = function() require("tmux").setup() end,
-    }
+    use "aserowy/tmux.nvim"
 end
 
-local function ensure_packer()
+function M.setup()
+    local freshly_installed = M.ensure_packer()
+    local packer = require("packer")
+
+    packer.startup(function(use)
+        M.setup_plugins(use)
+        if freshly_installed then packer.sync() end
+    end)
+end
+
+function M.ensure_packer()
     local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
     if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -103,14 +94,4 @@ local function ensure_packer()
     end
 end
 
-return {
-    setup = function()
-        local freshly_installed = ensure_packer()
-        local packer = require("packer")
-
-        packer.startup(function(use)
-            M.setup_packer(use)
-            if freshly_installed then packer.sync() end
-        end)
-    end
-}
+return M
