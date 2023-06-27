b8cea2546ddda8b391061292b128726ce48efb69
Brian Rodriguez <brianrz@amazon.com>
2023-06-27 19:17:54 -0400

    Show OS in vim

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,4 +1,17 @@
+local function get_os()
+	if vim.fn.has("unix") then
+		return ""
+	elseif vim.fn.has("win32") or vim.fn.has("win32unix") then
+		return ""
+	elseif vim.fn.has("Macintosh") then
+		return ""
+	end
+end
+
 require("lualine").setup({
 	extensions = { "fugitive", "nvim-tree" },
+	sections = {
+		lualine_a = { get_os, "FugitiveHead" },
+	},
 	theme = "gruvbox",
 })
