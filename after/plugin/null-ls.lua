3ca97d85d66403252036cacf660d42db661b16ba
Brian Rodriguez <brianrz@amazon.com>
2023-06-27 19:02:06 -0400

    Add google java format

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -5,6 +5,7 @@ null_ls.setup({
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.completion.spell,
 		null_ls.builtins.diagnostics.luacheck,
+		null_ls.builtins.formatting.google_java_format,
 		null_ls.builtins.formatting.stylua,
 	},
 })
