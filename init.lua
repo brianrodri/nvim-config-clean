39c08da4be99db248fafe0cf078c8323e98dc534
Brian Rodriguez <brian@brianrodri.com>
2023-07-01 07:46:12 -0400

    Move packer init to init.vim

--- init.lua
+++ init.lua
@@ -1,4 +1,28 @@
-require("my.packer")
-require("my.options").setup()
-require("my.remaps").setup()
-require("my.lsp").setup()
+local function ensure_packer()
+	local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+		vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd.packadd("packer.nvim")
+		return true
+	else
+		return false
+	end
+end
+
+local freshly_installed = ensure_packer()
+local packer = require("packer")
+
+vim.api.nvim_create_autocmd("User PackerComplete", {
+	group = vim.api.nvim_create_augroup("UserPackerHook", { clear = true }),
+	callback = function()
+		if freshly_installed then
+			packer.sync()
+		end
+		require("my.options").setup()
+		require("my.remaps").setup()
+		require("my.lsp").setup()
+	end,
+})
+
+packer.startup(function(use) require("my.packer").setup_plugins(use) end)
--- lua/my/packer.lua
+++ lua/my/packer.lua
@@ -215,32 +215,4 @@ function M.setup_tmux_plugins(use)
 	})
 end
 
-local function ensure_packer()
-	local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-		vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd.packadd("packer.nvim")
-		return true
-	else
-		return false
-	end
-end
-
-local freshly_installed = ensure_packer()
-
-local packer = require("packer")
-packer.startup(function(use)
-	M.setup_plugins(use)
-	if freshly_installed then
-		packer.sync()
-	end
-end)
-
--- Auto-sync packer when this file changes.
-vim.cmd([[
-augroup packer_user_config
-autocmd!
-autocmd BufWritePost packer.lua source <afile> | PackerSync
-augroup end
-]])
+return M
