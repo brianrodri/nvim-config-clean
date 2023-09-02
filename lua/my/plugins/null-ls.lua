f9107457217a2f5a60abbbd714eb4de78ddba84a
Brian Rodriguez <brian@brianrodri.com>
2023-09-01 21:32:53 -0400

    Stop using eslint-d

--- lua/my/plugins/null-ls.lua
+++ lua/my/plugins/null-ls.lua
@@ -2,12 +2,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.code_actions.eslint_d,
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.completion.spell,
-		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.luacheck,
-		null_ls.builtins.formatting.eslint_d,
 		null_ls.builtins.formatting.google_java_format,
 		null_ls.builtins.formatting.stylua,
 	},
